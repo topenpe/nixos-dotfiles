15befd8c747b9159418978c61bc8b41e4e17ec61
Dmitry Chekmarev-Schreiner <110166186+topenpe@users.noreply.github.com>
2025-01-18 19:44:32 +0400

    Set up wlsunset

--- home-manager/modules/desktop.nix
+++ home-manager/modules/desktop.nix
@@ -6,10 +6,12 @@
     ./submodules/gtk.nix
     ./submodules/hyprland.nix
     ./submodules/nushell.nix
+    ./submodules/wlsunset.nix
   ];
 
   eww.enable = true;
   gtkConfig.enable = lib.mkDefault true;
   hyprConfig.enable = lib.mkDefault true;
   nushellConfig.enable = false;
+  wlsunsetConfig.enable = true;
 }
---
+++ home-manager/modules/submodules/wlsunset.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  config,
+  ...
+}:
+
+{
+  options.wlsunsetConfig.enable = lib.mkEnableOption "Enable wlsunset";
+
+  config = lib.mkIf config.wlsunsetConfig.enable {
+    services.wlsunset = {
+      enable = true;
+      sunrise = "8:25";
+      sunset = "18:00";
+      temperature = {
+        day = 6500;
+        night = 4000;
+      };
+    };
+  };
+}
